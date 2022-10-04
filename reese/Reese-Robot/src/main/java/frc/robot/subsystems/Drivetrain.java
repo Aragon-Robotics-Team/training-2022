57aec45b405414bf82d6ea9db138673be9067b55
Roxydog21 <reesepavo@gmail.com>
2022-10-04 16:19:28 -0700

    cleaned up and added comments
    I added comments.

--- reese/Reese-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ reese/Reese-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,14 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+// importing stuff
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-  /** Creates a new Drivetrain. */
+  /** Creates a new Drivetrain. **/
   private static final class Config {
     public static final int krightPrimaryID = 0;
     public static final int kleftPrimaryID = 1;
@@ -24,13 +24,15 @@ public class Drivetrain extends SubsystemBase {
   private WPI_TalonFX m_rightSecondMotor = new WPI_TalonFX(Config.krightSecondID);
   private WPI_TalonFX m_leftSecondMotor = new WPI_TalonFX(Config.krightSecondID);
   public Drivetrain() {
+   //so motors follow eachother
     m_leftSecondMotor.follow(m_leftPrimaryMotor);
     m_rightSecondMotor.follow(m_rightSecondMotor);
-
+    //Setting Inverted
     m_leftSecondMotor.setInverted(true);
     m_rightPrimaryMotor.setInverted(true);
     
   }
+  //setting speed
   public void setrightPrimarySpeed(double speed){
     m_rightPrimaryMotor.set(speed);
   }
@@ -38,7 +40,7 @@ public class Drivetrain extends SubsystemBase {
   public void setleftPrimarySpeed(double speed){
     m_rightPrimaryMotor.set(speed);
   }
-
+//setting neutral
 public void setIdleeMode(NeutralMode idle){
 m_leftSecondMotor.setNeutralMode(idle);
 m_rightSecondMotor.setNeutralMode(idle);
@@ -54,4 +56,4 @@ m_leftPrimaryMotor.setNeutralMode(idle);
     }
   }
 
-}
+
