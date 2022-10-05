26a9c429af12873f338cb26a443c92e8fbda6695
jakehu2008 <61633287+jakehu2008@users.noreply.github.com>
2022-10-04 18:29:59 -0700

    I did work for 10/4/22
    Joystick is joystick and I did joystick

---
+++ jake/Jake-robot/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.Drivetrain;
+
+public class ArcadeDrive extends CommandBase {
+  private static final class Config {
+    public static final int k_rightStickY = 0;
+    public static final int k_rightStickX = 0;
+  }
+
+  private Drivetrain m_drivetrain;
+  private Joystick m_joystick;
+
+  public ArcadeDrive(Drivetrain drivetrain, Joystick joystick) {
+    m_drivetrain = drivetrain;
+    m_joystick = joystick;
+    addRequirements(m_drivetrain);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
