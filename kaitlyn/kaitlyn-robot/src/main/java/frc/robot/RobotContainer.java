ed8a3e00b3279ce1a680b73cded4021735a220d8
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2022-10-06 18:30:07 -0700

    Finished ArcadeDrive (Kaitlyn)
    Finished ArcadeDrive and added Teleop commands to Robot.java and RobotContainer.java

--- kaitlyn/kaitlyn-robot/src/main/java/frc/robot/RobotContainer.java
+++ kaitlyn/kaitlyn-robot/src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +19,21 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private static final class Config {
+   public static final int kJoystickID = 0;
+  }
+  private Joystick m_joystick = new Joystick(Config.kJoystickID);
+  private Drivetrain m_drivetrain = new Drivetrain();
+  private ArcadeDrive m_arcadeDrive = new ArcadeDrive(m_drivetrain, m_joystick);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    
+    
   }
 
   /**
@@ -43,6 +51,11 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
+  }
+
+  public Command getTeleopCommand(){
+    m_drivetrain.setDefaultCommand(m_arcadeDrive);
+    return null;
   }
 }
