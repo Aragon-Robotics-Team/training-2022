8c98ad401495b1d76da8a769bbe4f0a41ba15fab
Charles20199 <113481961+Charles20199@users.noreply.github.com>
2022-10-06 18:28:33 -0700

    made commands

--- charles/Charles-Robot/src/main/java/frc/robot/RobotContainer.java
+++ charles/Charles-Robot/src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,17 +19,26 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private static final class Config{
+     public static final int joystickID = 0;
+  
+
+  }
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+ 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private Drivetrain m_drivetrain = new Drivetrain();
+  private Joystick m_stick = new Joystick(Config.joystickID);
+  private ArcadeDrive m_ArcadeDrive= new ArcadeDrive(m_drivetrain, m_stick);
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-  }
+   
+  } 
 
+
+    
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -41,8 +52,12 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+  public Command getAutonomousCommand(){
+
   }
+  public Command getTeleopCommand(){
+    m_drivetrain.setDefaultCommand(ArcadeDrive);
+    return null;
+  }
+  
 }
