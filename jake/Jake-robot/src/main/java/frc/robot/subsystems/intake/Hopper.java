eccc8e5bc893ec843f78016badb69fbfb1b4f975
jakehu2008 <61633287+jakehu2008@users.noreply.github.com>
2022-10-20 18:11:07 -0700

    Hopper and Intake arm
    We did Hopper Intake arm today

--- jake/Jake-robot/src/main/java/frc/robot/subsystems/intake/Hopper.java
+++ jake/Jake-robot/src/main/java/frc/robot/subsystems/intake/Hopper.java
@@ -7,19 +7,47 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hopper extends SubsystemBase {
   /** Creates a new Hopper. */
   private static final class Config {
     public static final int kHopperMotorID = 10;
-  }
-  public Hopper() {
-    
+    public static final double kHopperSpeed = 0.7;
   }
 
   private WPI_TalonFX m_hopperMotor = new WPI_TalonFX(Config.kHopperMotorID);
 
+  public Hopper() {
+    m_hopperMotor.setInverted(true);
+    
+  }
+  // setting forward speed
+  public void setForwardHopperSpeed(){
+    m_hopperMotor.set(Config.kHopperSpeed);
+  }
+  // setting reverse speed
+  public void setReverseHopperSpeed(){
+    m_hopperMotor.set(-Config.kHopperSpeed);
+  }
+  
+  public void setZeroHopperSpeed(){
+    m_hopperMotor.set(0.0);
+  }
+
+  public InstantCommand HopperForward(){
+    return new InstantCommand(this::setForwardHopperSpeed, this);
+  }
+
+
+  public InstantCommand HopperReverse(){
+    return new InstantCommand(this::HopperReverse, this);
+  }
+
+  public InstantCommand HopperOff(){
+    return new InstantCommand(this::HopperOff, this);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
