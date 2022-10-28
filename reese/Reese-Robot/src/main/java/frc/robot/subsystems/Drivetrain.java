8f44c4c9b9d10cc88fa24ddd78aaded7de9db4de
Roxydog21 <reesepavo@gmail.com>
2022-10-27 17:56:15 -0700

    fixed errors

--- reese/Reese-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ reese/Reese-Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,24 +12,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. **/
   private static final class Config {
-    public static final int krightPrimaryID = 4;
-    public static final int kleftPrimaryID = 2;
+    public static final int krightPrimaryID = 2;
+    public static final int kleftPrimaryID = 4;
     public static final int krightSecondID = 1;
     public static final int kleftSecondID = 3;
   }
   
   // making motors IDs
   private WPI_TalonFX m_rightPrimaryMotor = new WPI_TalonFX(Config.krightPrimaryID);
-  private WPI_TalonFX m_leftPrimaryMotor = new WPI_TalonFX(Config.krightPrimaryID);
+  private WPI_TalonFX m_leftPrimaryMotor = new WPI_TalonFX(Config.kleftPrimaryID);
   private WPI_TalonFX m_rightSecondMotor = new WPI_TalonFX(Config.krightSecondID);
-  private WPI_TalonFX m_leftSecondMotor = new WPI_TalonFX(Config.krightSecondID);
+  private WPI_TalonFX m_leftSecondMotor = new WPI_TalonFX(Config.kleftSecondID);
   public Drivetrain() {
    //so motors follow eachother
     m_leftSecondMotor.follow(m_leftPrimaryMotor);
-    m_rightSecondMotor.follow(m_rightSecondMotor);
+    m_rightSecondMotor.follow(m_rightPrimaryMotor);
     //Setting Inverted
     m_leftSecondMotor.setInverted(true);
-    m_rightPrimaryMotor.setInverted(true);
+    m_leftPrimaryMotor.setInverted(true);
     
   }
   //setting speed
