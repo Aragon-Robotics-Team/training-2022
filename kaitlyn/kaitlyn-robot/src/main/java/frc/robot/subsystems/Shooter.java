9ad735574e81395f774ce4757287ffa63b01db08
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2022-11-01 18:23:58 -0700

    Finished Shooter Commands and Subsystems (Kaitlyn)
    Completed the shooter subsystem and added the related shooter commands

--- kaitlyn/kaitlyn-robot/src/main/java/frc/robot/subsystems/Shooter.java
+++ kaitlyn/kaitlyn-robot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,12 +4,53 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxLimitSwitch.Type;
+
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
 
-  public Shooter() {}
+  private static final class Config{
+    public static final int kShooterMotorID = 7;
+    public static final int kServo = 9;
+  }
+
+  public static final double kRunningSpeed = 0.5;
+
+  private CANSparkMax m_shooterMotor = new CANSparkMax(Config.kShooterMotorID, MotorType.kBrushless);
+  private Servo m_servo = new Servo(Config.kServo);
+
+  public Shooter() {
+    m_shooterMotor.setInverted(false);
+  };
+
+  public void setMotor(double speed){
+    m_shooterMotor.set(speed);
+  }
+
+  public void openServo(){
+    m_servo.set(0.0);
+  }
+
+  public void closeServo(){
+    m_servo.set(1.0);
+  }
+
+  public boolean getUpperLimit(){
+    //Limit switches are connected to the motor
+    //Normally closed = returning true, press = returning false
+    //Is pressed = is limit switch pressed
+    return m_shooterMotor.getReverseLimitSwitch(Type.kNormallyClosed).isPressed();
+  }
+
+  public boolean getLowerLimit(){
+    return m_shooterMotor.getForwardLimitSwitch(Type.kNormallyClosed).isPressed();
+  }
 
   @Override
   public void periodic() {
