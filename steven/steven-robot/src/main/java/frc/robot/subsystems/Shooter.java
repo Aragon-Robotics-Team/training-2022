ddc7cb97fcd06629c39bd5cdc6159195fa11f14f
LightLord <87213208+LightLordYT@users.noreply.github.com>
2022-11-03 17:15:41 -0700

    shooter stuff

--- steven/steven-robot/src/main/java/frc/robot/subsystems/Shooter.java
+++ steven/steven-robot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,11 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxLimitSwitch.Type;
+
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+  private static final class Config {
+    public static final int k_motorId = 7;
+    public static final int k_servoId = 9;
+  }
+  public final double k_runningSpeed = 0.5;
+  private Servo m_servo = new Servo(Config.k_servoId);
   /** Creates a new Shooter. */
-  public Shooter() {}
+  private CANSparkMax m_motor = new CANSparkMax(Config.k_motorId, MotorType.kBrushless);
+
+  public void setMotorSpeed(double speed){
+    m_motor.set(speed);
+  }
+
+  public boolean getUpperLimit(){
+    return m_motor.getReverseLimitSwitch(Type.kNormallyClosed).isPressed();
+  }
+
+  public boolean getLowerLimit(){
+    return m_motor.getForwardLimitSwitch(Type.kNormallyClosed).isPressed();
+  }
+
+  public void openServo(){
+    m_servo.set(0);
+  }
+
+  public void closeServo(){
+    m_servo.set(1);
+  }
+
+  public Shooter() {
+    m_motor.setInverted(false);
+  }
 
   @Override
   public void periodic() {
