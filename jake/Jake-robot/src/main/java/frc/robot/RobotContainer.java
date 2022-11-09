9bcdf66b532244aef74be74776c78c3f254c2709
jakehu2008 <61633287+jakehu2008@users.noreply.github.com>
2022-11-08 16:33:56 -0800

    Climb Command
    Climb Command

--- jake/Jake-robot/src/main/java/frc/robot/RobotContainer.java
+++ jake/Jake-robot/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ArcadeDrive;
+        
 import frc.robot.commands.RunIntake;
 import frc.robot.commands.Shooting.Launch;
 import frc.robot.commands.Shooting.Reload;
--- jake/Jake-robot/src/main/java/frc/robot/subsystems/Climb.java
+++ jake/Jake-robot/src/main/java/frc/robot/subsystems/Climb.java
@@ -4,12 +4,38 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climb extends SubsystemBase {
-  /** Creates a new Climb. */
-  public Climb() {}
+  private static final class Config{
+    public static final int kClimbID = 0;
 
+  }
+
+  private Solenoid m_climb = new Solenoid(1, PneumaticsModuleType.CTREPCM, 5);
+  
+  public void climbOut(){
+    m_climb.set(true);
+  }
+
+  public void climbIn(){
+    m_climb.set(false);
+  }
+
+  public InstantCommand climbIn1(){
+    return new InstantCommand(this::climbIn, this);
+  }
+
+  public InstantCommand climbOut1(){
+    return new InstantCommand(this::climbOut, this);
+  }
+
+  public boolean climbState(){
+    return m_climb.get();
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
